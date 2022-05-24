3b7c8d9e41901bffac408eaf917058a1702c8647
Unyielding1 <david.verde.alvarez@gmail.com>
2022-05-23 19:06:17 -0500

    add and remove function

--- script.js
+++ script.js
@@ -1,37 +1,59 @@
+const bookList = document.getElementById('book-list');
+const addBook = document.getElementById('add-book');
+const newTitle = document.getElementById('new-title');
+const newAuthor = document.getElementById('new-author');
+let bookData = [];
+class Book {
+  constructor(title = 'book', author = 'people', id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
+}
 
-    const bookTitle = document.getElementById('title');
-    const bookAuthor = document.getElementById('author');
-    const addBtn = document.getElementById('add');
-    const booksSection = document.getElementById('mybooks');
-    const awsome_book_list = [];
+function getLi(title, author, id) {
+  const divAuthor = document.createElement('div');
+  const divTitle = document.createElement('div');
+  const removeButton = document.createElement('button');
+  const hr = document.createElement('hr');
+  const li = document.createElement('li');
 
-    let books = {
-      title: "",
-      author: ""
-    }
+  divTitle.classList.add('author');
+  divAuthor.classList.add('title');
+  removeButton.classList.add('remove');
+  removeButton.setAttribute('id', `button${id}`);
+  removeButton.setAttribute('onclick', `javascript:removeLi(${id})`);
+  li.classList.add('book');
+  li.setAttribute('id', `book${id}`);
 
-    function populteSection(arr) {
-      const bookArticle = document.createElement('article');
-      for(let i = 0; i < arr.length; i += 1) {
-        bookArticle.innerHTML = `<span>${arr[i].title}</span> <br>
-        <span>${arr[i].author}</span><br>
-        <button>Remove</button>
-        <hr>`
-      }
-      booksSection.appendChild(bookArticle);
-    }
-    
+  divTitle.innerHTML = title;
+  divAuthor.innerHTML = author;
+  removeButton.innerText = 'Remove';
+  removeButton.type = 'button';
 
-    function add(title, author) {
-      books.title = title;
-      books.author = author;
-      awsome_book_list.push(books);
-    }
+  li.appendChild(divTitle);
+  li.appendChild(divAuthor);
+  li.appendChild(removeButton);
+  li.appendChild(hr);
 
-    addBtn.addEventListener('click', () => {
-      add(bookTitle.value,bookAuthor.value);
-      populteSection(awsome_book_list);
-      console.log(awsome_book_list);
-    });
-  
-    
+  return li;
+}
+
+addBook.addEventListener('click', () => {
+  if (newTitle.value && newAuthor.value) {
+    const id = bookData[bookData.length - 1] ? bookData[bookData.length - 1].id + 1 : 1;
+    const book = new Book(newTitle.value, newAuthor.value, id);
+    bookData.push(book);
+    bookList.appendChild(getLi(book.title, book.author, book.id));
+    storeData();
+  }
+});
+
+function removeLi(id) {
+  const li = document.getElementById(`book${id}`);
+  li.remove();
+  bookData = bookData.filter((book) => book.id !== id);
+  storeData();
+}
+
+removeLi(0);
